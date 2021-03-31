c4034eeaccd812e19c9029c8f610a04cb09d1a83
Evenstein <404notfounD404notfounD>
2021-03-31 15:07:33 +0300

    [Task-3] Created tests for sorting, output to console.

---
+++ lib/actions/sort.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Sort
+  def sort_by_count(hash)
+
+  end
+
+  def sort_by_uniq_count(hash)
+
+  end
+end
---
+++ lib/output/result_output.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# This module provides methods for print out results to console.
+module ResultOutput
+  def console_output(info, ending = nil)
+
+  end
+end
