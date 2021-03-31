1726c362e54a6fbec28169303374fb1f8a1be88f
Evenstein <404notfounD404notfounD>
2021-03-31 14:07:38 +0300

    [Task-1] Created CL arguments validation.

--- lib/validators/argument_validator.rb
+++ lib/validators/argument_validator.rb
@@ -14,6 +14,38 @@ class ArgumentValidator
   end
 
   def valid?
+    argument_present? && file_exists? && file_has_content? && right_format?
+  end
 
+  private
+
+  def argument_present?
+    return true if @path
+
+    raise_error(ARGUMENT_EMPTY)
+  end
+
+  def file_exists?
+    return true if File.exist?(@path)
+
+    raise_error(FILE_NOT_EXISTS)
+  end
+
+  def file_has_content?
+    return true unless @path && File.zero?(@path)
+
+    raise_error(FILE_EMPTY)
+  end
+
+  def right_format?
+    return true if @path && VALID_FILE_FORMATS.include?(File.extname(@path))
+
+    raise_error(WRONG_FORMAT)
+  end
+
+  def raise_error(message)
+    @error_message = message
+
+    false
   end
 end
