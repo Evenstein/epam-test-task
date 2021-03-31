4122361db7a0f3b2151445a81e51bb42519bce36
Evenstein <404notfounD404notfounD>
2021-03-31 14:54:28 +0300

    [Task-2] Created file process handler, entities (history, line), file content validators (query and ipv4).

--- lib/entities/history.rb
+++ lib/entities/history.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# This class represents an history of server requests
 class History
+  attr_reader :log
 
+  def initialize
+    @log = []
+  end
+
+  def log_history(item)
+    @log << item
+  end
 end
--- lib/parser.rb
+++ lib/parser.rb
@@ -6,5 +6,8 @@ module Parser
   def parse(path = nil)
     validator = ArgumentValidator.new(path)
     return puts validator.error_message unless validator.valid?
+
+    handler = QueryHandler.new
+    File.open(path.to_s, 'r').each { |line| handler.process_line(line) }
   end
 end
