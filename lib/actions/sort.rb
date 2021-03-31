0667881c6b7cb024782a718e9b8a091c5367823f
Evenstein <404notfounD404notfounD>
2021-03-31 15:21:31 +0300

    [Task-3] Created sorting, output to console. Update styles

--- lib/actions/sort.rb
+++ lib/actions/sort.rb
@@ -2,10 +2,13 @@
 
 module Sort
   def sort_by_count(hash)
-
+    hash.sort_by { |_k, value| value.size }.reverse.to_h
   end
 
   def sort_by_uniq_count(hash)
-
+    hash.sort_by do |_k, value|
+      value.uniq!
+      value.size
+    end.reverse.to_h
   end
 end
--- lib/output/result_output.rb
+++ lib/output/result_output.rb
@@ -3,6 +3,13 @@
 # This module provides methods for print out results to console.
 module ResultOutput
   def console_output(info, ending = nil)
+    return puts info unless info.is_a? Hash
 
+    output_hash(info, ending)
+  end
+
+  def output_hash(hash, ending)
+    puts '-----------------------------------'
+    hash.each { |k, v| puts "#{k} #{v.size} #{ending}" }
   end
 end
